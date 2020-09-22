4b2834d872f43f270f192aa02d091bdc59f5cbd1
Holbootla <holbootla@gmail.com>
2020-09-22 13:34:48 +0300

    feat: add log-functions for click buttons

--- calculator/script.js
+++ calculator/script.js
@@ -4,24 +4,46 @@ const allClear = document.querySelector('[data-all-clear]');
 const del = document.querySelector('[data-delete]');
 const result = document.querySelector('[data-equals]');
 
-
-
-
-function numberPress() {
-
+const numberPress = () => {
+    console.log('Клик по кнопке с номером');
 };
 
-function operation() {
-
+const operationPress = () => {
+    console.log('Клик по кнопке с операцией');
 };
 
-function decimal() {
-
+const acPress = () => {
+    console.log('Клик по кнопке AC');
 };
 
-function clear() {
-
+const delPress = () => {
+    console.log('Клик по кнопке DEL');
 };
+
+const resultPress = () => {
+    console.log('Клик по кнопке =');
+};
+
+
+
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    number.addEventListener('click', numberPress);
+}
+
+for (let i = 0; i < operations.length; i++) {
+    let operation = operations[i];
+    operation.addEventListener('click', operationPress);
+}
+
+allClear.addEventListener('click', acPress);
+
+del.addEventListener('click', delPress);
+
+result.addEventListener('click', resultPress);
+
+
+
 
 
 
