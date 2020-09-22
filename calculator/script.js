1620219524c675c75ba1353bf44ed5e32004631e
Holbootla <holbootla@gmail.com>
2020-09-22 15:53:07 +0300

    feat: add math operations

--- calculator/script.js
+++ calculator/script.js
@@ -4,15 +4,51 @@ const allClear = document.querySelector('[data-all-clear]');
 const del = document.querySelector('[data-delete]');
 const result = document.querySelector('[data-equals]');
 
-const numberPress = () => {
-    console.log('Клик по кнопке с номером');
+let display = document.getElementById('display');
+let currentNumber = 0;
+let newNumber = false;
+let pendingOperation = '';
+
+const numberPress = (symbol) => {
+    if (newNumber) {
+        display.value = symbol;
+        newNumber = false;
+    } else {
+        if (display.value === '0') {
+            display.value = symbol;
+        } else {
+            display.value += symbol;
+        }
+    }
+    console.log(`Клик по кнопке с номером ${symbol}`);
 };
 
-const operationPress = () => {
-    console.log('Клик по кнопке с операцией');
+const operationPress = (op) => {
+    let localOperationMemory = display.value;
+
+    if (newNumber === true && pendingOperation != '=') {
+        display.value = currentNumber;
+    } else {
+        newNumber = true;
+        if (pendingOperation === '+') {
+            currentNumber += parseFloat(localOperationMemory);
+        } else if (pendingOperation === '-') {
+            currentNumber -= parseFloat(localOperationMemory);
+        } else if (pendingOperation === '*') {
+            currentNumber *= parseFloat(localOperationMemory);
+        } else if (pendingOperation === '/') {
+            currentNumber /= parseFloat(localOperationMemory);
+        } else {
+            currentNumber = parseFloat(localOperationMemory);
+        }
+        display.value = currentNumber;
+        pendingOperation = op;
+    }
+    console.log(`Клик по кнопке с операцией ${op}`);
 };
 
 const acPress = () => {
+    display.value = '0';
     console.log('Клик по кнопке AC');
 };
 
@@ -20,28 +56,24 @@ const delPress = () => {
     console.log('Клик по кнопке DEL');
 };
 
-const resultPress = () => {
-    console.log('Клик по кнопке =');
-};
-
-
-
 for (let i = 0; i < numbers.length; i++) {
     let number = numbers[i];
-    number.addEventListener('click', numberPress);
+    number.addEventListener('click', function(e) {
+        numberPress(e.target.textContent);
+    });
 }
 
 for (let i = 0; i < operations.length; i++) {
     let operation = operations[i];
-    operation.addEventListener('click', operationPress);
+    operation.addEventListener('click', function(e) {
+        operationPress(e.target.textContent);
+    });
+   
 }
 
 allClear.addEventListener('click', acPress);
 
 del.addEventListener('click', delPress);
-
-result.addEventListener('click', resultPress);
-
 
 
 
