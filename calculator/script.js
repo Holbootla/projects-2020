e8b3f224f776bbd3c3c62ee16832ae8181f9813f
Holbootla <holbootla@gmail.com>
2020-09-22 10:49:56 +0300

    refactor: change html, css, initialize main variables

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+const numbers = document.querySelectorAll('[data-number]');
+const operations = document.querySelectorAll('[data-operation');
+const allClear = document.querySelector('[data-all-clear]');
+const del = document.querySelector('[data-delete]');
+const result = document.querySelector('[data-equals]');
+
+
+
+
+function numberPress() {
+
+};
+
+function operation() {
+
+};
+
+function decimal() {
+
+};
+
+function clear() {
+
+};
+
+
+
