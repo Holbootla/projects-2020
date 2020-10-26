7d4983f8aa7643b8a0743e448799ef8367d89edd
Holbootla <holbootla@gmail.com>
2020-10-26 16:42:16 +0300

    feat: add popup to main page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -28,3 +28,28 @@ function closeBurgerMenu() {
     document.body.classList.remove('overflow-hidden');
     header.classList.remove('background-none');
 }
+
+
+// constructing slides from JSON
+
+const petsCardsContainer = document.querySelector('.pets-cards');
+const petButton = document.querySelector('.pet-button');
+
+async function getPets() {  
+    const source = `../../assets/data/pets.json`;
+    const res = await fetch(source);
+    const data = await res.json();
+
+    // shuffle of data array
+    for (let i = data.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [data[i], data[j]] = [data[j], data[i]];
+      }
+
+    for (i = 0; i < data.length; i++) {
+        petsCardsContainer.insertAdjacentHTML('beforeend', `<div class="pet-card"><img src="${data[i].img}"><h4 class="pet-card-title">${data[i].name}</h4><div class="button-secondary pet-button" id="${data[i].name}">Learn more</div></div>`    )
+    }
+          
+}
+
+getPets();
